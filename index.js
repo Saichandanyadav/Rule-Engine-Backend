58e513b2a7c593c52a00aabad523de9c6c2b5e2d
Sai Chandan <Saichandhanyadav2002@gmail.com>
2024-10-19 18:42:59 +0530

    Updated File

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const rulesRoutes = require('./routes/ruleRoutes');
-const cors = require('cors'); // Importing CORS
+const cors = require('cors'); 
 
 dotenv.config();
 
-const app = express(); // Create the Express app
+const app = express(); 
 
-// Use CORS middleware
-app.use(cors()); // This should be before other middleware/routes
+
+app.use(cors()); 
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to the database
+
 connectDB();
 
-// Middleware to parse JSON requests
+
 app.use(express.json()); 
 
-// Routes
+
 app.use('/api/rules', rulesRoutes);
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
     console.error(err.stack); 
     res.status(500).send({ message: 'Something went wrong!' }); // Generic error response
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
